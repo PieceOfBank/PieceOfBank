723a62bd28841202634eda033411b39d9871a584
주연수 <jooys130@naver.com>
2024-09-10 13:10:19 +0900

    ✨feat: User Entity 추가

---
+++ backend/src/main/java/com/fintech/pob/domain/user/entity/User.java
@@ -0,0 +1,37 @@
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "user_key", unique = true, nullable = false, updatable = false)
+    private UUID userKey;
+
+    @Column(name = "user_name", nullable = false, length = 50)
+    private String userName;
+
+    @Column(name = "user_password", nullable = false)
+    private String userPassword;  // 6자리 PIN 암호화하여 저장
+
+    @Column(name = "created", nullable = false, updatable = false)
+    private LocalDateTime created;
+
+    @Column(name = "updated", nullable = false)
+    private LocalDateTime updated;
+
+    @Column(name = "subscription_type", nullable = false)
+    private int subscriptionType; // 0: Default, 1: 보호자, 2: 피보호자
+
+    @PrePersist
+    protected void onCreate() {
+        this.created = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        this.updated = LocalDateTime.now();
+    }
+}
