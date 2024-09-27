e2362400002b8f995d580c06dc865b08cb2e0fb2
jooys130 <wndustn130@gmail.com>
2024-09-27 17:03:00 +0900

    🎨feat: 푸시 알림 전송

---
+++ backend/src/main/java/com/fintech/pob/domain/notification/controller/NotificationController.java
@@ -0,0 +1,37 @@
+package com.fintech.pob.domain.notification.controller;
+
+import com.fintech.pob.domain.notification.dto.NotificationRequestDto;
+import com.fintech.pob.domain.notification.service.NotificationService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+
+import java.io.IOException;
+
+@Slf4j
+@RestController
+@RequestMapping("/notification")
+public class NotificationController {
+
+    private final NotificationService notificationService;
+
+    public NotificationController(NotificationService notificationService) {
+        this.notificationService = notificationService;
+    }
+
+    @PostMapping("/send")
+    public Mono<ResponseEntity<Integer>> pushMessage(@RequestBody @Validated NotificationRequestDto notificationRequestDto) throws IOException {
+        log.debug("[+] 푸시 메시지를 전송합니다.");
+        // 비동기적으로 NotificationService의 sendMessageTo 메서드를 호출하고, Mono를 반환하도록 변경
+        return notificationService.sendMessageTo(notificationRequestDto)
+                .map(result -> new ResponseEntity<>(result, HttpStatus.OK)) // 성공 시 OK 응답
+                .doOnError(e -> log.error("푸시 메시지 전송 중 에러 발생: {}", e.getMessage())) // 에러 로그
+                .onErrorResume(e -> Mono.just(new ResponseEntity<>(0, HttpStatus.INTERNAL_SERVER_ERROR))); // 실패 시 500 응답, 0 반환
+    }
+}
