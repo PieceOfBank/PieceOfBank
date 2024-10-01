162eb1b2ac52b3b5240c01dcea7c32336f8661ff
jooys130 <wndustn130@gmail.com>
2024-10-02 01:08:09 +0900

    ♻️refactor: 책임 분리

--- backend/src/main/java/com/fintech/pob/domain/notification/controller/NotificationController.java
+++ backend/src/main/java/com/fintech/pob/domain/notification/controller/NotificationController.java
@@ -3,6 +3,7 @@ package com.fintech.pob.domain.notification.controller;
 import com.fintech.pob.domain.notification.dto.NotificationRequestDto;
 import com.fintech.pob.domain.notification.dto.TransactionApprovalRequestDto;
 import com.fintech.pob.domain.notification.dto.TransactionApprovalResponseDto;
+import com.fintech.pob.domain.notification.service.FCMService;
 import com.fintech.pob.domain.notification.service.NotificationService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
@@ -19,9 +20,11 @@ import java.io.IOException;
 public class NotificationController {
 
     private final NotificationService notificationService;
+    private final FCMService fcmService;
 
-    public NotificationController(NotificationService notificationService) {
+    public NotificationController(NotificationService notificationService, FCMService fcmService) {
         this.notificationService = notificationService;
+        this.fcmService = fcmService;
     }
 
     @PostMapping("/transfers/request")
@@ -40,7 +43,7 @@ public class NotificationController {
     @PostMapping("/send")
     public Mono<ResponseEntity<Integer>> pushMessage(@RequestBody @Validated NotificationRequestDto notificationRequestDto) throws IOException {
         log.debug("[+] 푸시 메시지를 전송합니다.");
-        return notificationService.sendMessageTo(notificationRequestDto)
+        return fcmService.sendMessageTo(notificationRequestDto)
                 .map(result -> new ResponseEntity<>(result, HttpStatus.OK))
                 .doOnError(e -> log.error("푸시 메시지 전송 중 에러 발생: {}", e.getMessage()))
                 .onErrorResume(e -> Mono.just(new ResponseEntity<>(0, HttpStatus.INTERNAL_SERVER_ERROR))); // 실패 시 500 응답, 0 반환
