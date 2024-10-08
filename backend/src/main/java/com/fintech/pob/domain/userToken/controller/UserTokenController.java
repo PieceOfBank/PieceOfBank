bbd9b82cdd703b75f021112cc275ae7417b653ec
jooys130 <wndustn130@gmail.com>
2024-10-08 15:20:14 +0900

    ♻️refactor: device token 관련 반환 값 변경

--- backend/src/main/java/com/fintech/pob/domain/userToken/controller/UserTokenController.java
+++ backend/src/main/java/com/fintech/pob/domain/userToken/controller/UserTokenController.java
@@ -1,5 +1,6 @@
 package com.fintech.pob.domain.userToken.controller;
 
+import com.fintech.pob.domain.userToken.dto.UserTokenRequestDto;
 import com.fintech.pob.domain.userToken.service.UserTokenService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
@@ -19,20 +20,20 @@ public class UserTokenController {
     }
 
     @PostMapping()
-    public ResponseEntity<String> saveToken(@RequestParam("userKey") UUID userKey, @RequestParam("token") String token) {
-        userTokenService.saveUserToken(userKey, token);
+    public ResponseEntity<String> saveToken(@RequestBody UserTokenRequestDto userTokenRequestDto) {
+        userTokenService.saveUserToken(userTokenRequestDto.getUserKey(), userTokenRequestDto.getToken());
         return ResponseEntity.ok("토큰이 저장되었습니다.");
     }
 
-    @GetMapping()
-    public ResponseEntity<?> getToken(@RequestParam("userKey") UUID userKey) {
+    @GetMapping("/{userKey}")
+    public ResponseEntity<?> getToken(@PathVariable("userKey") UUID userKey) {
         return userTokenService.getUserTokenByUserKey(userKey)
                 .map(userToken -> ResponseEntity.ok(userToken.getToken()))
                 .orElse(ResponseEntity.badRequest().body("해당 유저에게 토큰이 없습니다." + userKey));
     }
 
-    @DeleteMapping()
-    public ResponseEntity<String> deleteToken(@RequestParam("userKey") UUID userKey) {
+    @DeleteMapping("/{userKey}")
+    public ResponseEntity<String> deleteToken(@PathVariable("userKey") UUID userKey) {
         userTokenService.deleteUserToken(userKey);
         return ResponseEntity.ok("토큰이 삭제되었습니다.");
     }
