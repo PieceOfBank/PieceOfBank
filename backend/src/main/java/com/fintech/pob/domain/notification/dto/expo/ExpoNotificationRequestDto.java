053044f6bd9538d80b6136eaecaad89080962d24
jooys130 <wndustn130@gmail.com>
2024-10-08 09:27:36 +0900

    🐛fix: dto 변경 및 문자열 변환

--- backend/src/main/java/com/fintech/pob/domain/notification/dto/expo/ExpoNotificationRequestDto.java
+++ backend/src/main/java/com/fintech/pob/domain/notification/dto/expo/ExpoNotificationRequestDto.java
@@ -13,7 +13,7 @@ import lombok.extern.slf4j.Slf4j;
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class ExpoNotificationRequestDto {
-    private String token;
+    private String to;
     private String title;
     private String content;
 }
