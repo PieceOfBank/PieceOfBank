06ee7aae19c1e5b0877ec3d278252deea77c9f42
inmyownway <wjdals05070@gmail.com>
2024-10-09 18:36:24 +0900

    :sparkles: feat: 구독 조회2

--- backend/src/main/java/com/fintech/pob/domain/subscription/controller/SubScriptionController.java
+++ backend/src/main/java/com/fintech/pob/domain/subscription/controller/SubScriptionController.java
@@ -13,7 +13,6 @@ import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.NoSuchElementException;
 import java.util.Optional;
 import java.util.UUID;
 
