5f4e4328814d7e21a9fc81cade9e38c27d11994b
김수빈 <86918962+ksb3458@users.noreply.github.com>
2024-10-10 17:28:59 +0900

    ✨feat: 대표 계좌 생성 메서드 호출 위치 변경

--- backend/src/main/java/com/fintech/pob/domain/account/service/account/AccountClientService.java
+++ backend/src/main/java/com/fintech/pob/domain/account/service/account/AccountClientService.java
@@ -3,6 +3,7 @@ package com.fintech.pob.domain.account.service.account;
 import com.fintech.pob.domain.account.entity.Account;
 import com.fintech.pob.domain.account.repository.AccountRepository;
 import com.fintech.pob.domain.user.entity.User;
+import com.fintech.pob.domain.user.service.LocalUserService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -13,12 +14,14 @@ import org.springframework.stereotype.Service;
 public class AccountClientService {
 
     private final AccountRepository accountRepository;
+    private final LocalUserService localUserService;
 
     public void saveAccount(User user, String accountNo) {
         Account account = new Account();
         account.setUser(user);
         account.setAccountNo(accountNo);
         accountRepository.save(account);
+        localUserService.updateAccountNo(user.getUserKey(), accountNo);
         System.out.println("---------end ---- saveAccount");
     }
 
